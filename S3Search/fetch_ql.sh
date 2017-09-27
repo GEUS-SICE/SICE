2795615126c7b2f5e48d5de53c78d8527d4ffaa8
Kenneth D. Mankoff <mankoff@gmail.com>
2017-09-27 15:29:54 +0200

    Fixed quicklook download bug

--- S3Search/fetch_ql.sh
+++ S3Search/fetch_ql.sh
@@ -3,6 +3,6 @@
 mkdir -p quicklook
 for ID in $(sort product_IDs.txt -k1,9 -t_ --stable --uniq | cut -d$'\t' -f2-); do
   echo $ID
-  URL="https://scihub.copernicus.eu/s3/odata/v1/Products('${ID}')/Products('Quicklook')/$value"
+  URL="https://scihub.copernicus.eu/s3/odata/v1/Products('${ID}')/Products('Quicklook')/\$value"
   wget "${URL}" --user=s3guest --password=s3guest -nc -c -nd -P ./quicklook -O ./quicklook/${ID}.jpg
 done
