745ecc89079deb7d2e65e631638b93be9d7f4f45
Kenneth D. Mankoff <mankoff@gmail.com>
2017-09-28 07:53:04 +0200

    Only fetch S3A_OL_1_EFR, not S3A_OL2_LFR

--- S3Search/fetch_ql.sh
+++ S3Search/fetch_ql.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 mkdir -p quicklook
-for ID in $(sort product_IDs.txt -k1,9 -t_ --stable --uniq | cut -d$'\t' -f2-); do
+for ID in $(grep EFR product_IDs.txt | sort -k1,9 -t_ --stable --uniq | cut -d$'\t' -f2-); do
   echo $ID
   URL="https://scihub.copernicus.eu/s3/odata/v1/Products('${ID}')/Products('Quicklook')/\$value"
   wget "${URL}" --user=s3guest --password=s3guest -nc -c -nd -P ./quicklook -O ./quicklook/${ID}.jpg
