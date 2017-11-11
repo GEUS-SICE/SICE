f6b0d7b1c963111ab7a3ebae498fddd8448b4e8a
Kenneth D. Mankoff <mankoff@gmail.com>
2017-11-11 20:19:13 +0100

    Improved patching

--- S3proc/dm.sh
+++ S3proc/dm.sh
@@ -16,9 +16,15 @@ OUTFOLDER=$3
 # INFOLDER=./tmp
 # OUTFOLDER=./mosaic
 
+###
+### Export code below comes from
+### https://grasswiki.osgeo.org/wiki/Working_with_GRASS_without_starting_it_explicitly#Bash_examples_.28GNU.2FLinux.29
+###
+
+## This is from "fink install grass72"
 export GISBASE=/sw/Applications/GRASS-mac-7.2.app/Contents/MacOS
 
-#generate GISRCRC
+## Work in /tmp/tmpG
 MYGISDBASE=/tmp
 MYLOC=tmpG
 MYMAPSET=PERMANENT
@@ -51,79 +57,82 @@ export MANPATH=$MANPATH:$GISBASE/man
 
 export GIS_LOCK=42
 
+###
+###
+###
+
 if ! [ -e "$INFOLDER" ]; then
   echo "$INFOLDER not found" >&2
   exit 1
 fi
 mkdir -p $OUTFOLDER/${DATE}
 
-# # load all the data
+# load all the data
 for ASCENE in $(cd $INFOLDER; find . -name "${DATE}T??????" -type d -depth 1); do
     SCENE=$(echo $ASCENE | cut -c3-)
-    g.mapset -c ${SCENE}
+    g.mapset -c ${SCENE} --quiet
     for file in $(ls ${INFOLDER}/${SCENE}/*.tif); do
-	echo "Importing $file"
-	band=$(echo $(basename ${file} .tif))
-	# echo $band
-	r.in.gdal input=${file} output=${band} --quiet
+        echo "Importing $file"
+        band=$(echo $(basename ${file} .tif))
+        # echo $band
+        r.in.gdal input=${file} output=${band} --quiet
 
-	# fix inf values in two of the rasters
-	if [[ ${band} == "albedo_broadband_planar" ]] || [[ ${band} == "albedo_broadband_spherical" ]]; then
-          r.null map=${band} setnull=inf --quiet
-	fi
+        # fix inf values in two of the rasters
+        if [[ ${band} == "albedo_broadband_planar" ]] || [[ ${band} == "albedo_broadband_spherical" ]]; then
+           r.null map=${band} setnull=inf --quiet
+        fi
     done
 done
 
+
+# The target bands. For example, Oa01_reflectance or SZA.
 BANDS=$(g.list type=raster mapset=* | cut -d"@" -f1 | sort | uniq)
 
 # set the region to include all the data
-g.mapset PERMANENT
+g.mapset PERMANENT --quiet
 g.region raster=$(g.list type=raster pattern=SZA separator=, mapset=*)
-g.region res=500 -a -p
-r.in.gdal input=mask.tif output=MASK
+g.region res=500 -a
+r.in.gdal input=mask.tif output=MASK --quiet
 g.region zoom=MASK
 g.region -s
-g.mapset -c ${DATE}
-
-# g.list type=raster pattern=SZA mapset=*
-SZA_arr=($(g.list type=raster pattern=SZA mapset=*))
-# echo ${SZA_arr[@]}
-SZA_list=$(g.list type=raster pattern=SZA mapset=* separator=comma)
+g.mapset -c ${DATE} --quiet
 
 # find the array index with the minimum SZA
+# Array for indexing, list for using in GRASS
+SZA_arr=($(g.list type=raster pattern=SZA mapset=*))
+SZA_list=$(g.list type=raster pattern=SZA mapset=* separator=comma)
 r.series input=${SZA_list} method=min_raster output=SZA_LUT --o
 
+# find the indices used. It is possible one scene is never used
 SZA_LUT_idxs=$(r.stats -n -l SZA_LUT)
 n_imgs=$(echo $SZA_LUT_idxs |wc -w)
 
-# make N temp rasters, one for each patch, each masked as appropriate
-for i in $SZA_LUT_idxs; do
-    echo $i
-    r.mask raster=SZA_LUT maskcats=${i} --o --q
-    g.region raster=MASK zoom=MASK
-    
-    # r.mapcalc "tmp_${i} = ${SZA_arr[${i}]}" --o
-    for B in $(echo ${BANDS}); do
-	# BAND_arr copied from the SZA_arr above:
-	echo $B
-	BAND_arr=($(g.list type=raster pattern=${B} mapset=*))
-	r.mapcalc "${B}_tmp_${i} = ${BAND_arr[${i}]}" --o
-    done
-done
+# Mask and zoom to Greenland ice+land
 r.mask raster=MASK@PERMANENT --o
 g.region -d
 g.region zoom=MASK@PERMANENT
 
-# patch the temp arrays to one mosaic and write to disk
+# Patch each BAND based on the minimum SZA_LUT
+for B in $(echo $BANDS); do
+    # this band in all of the sub-mapsets (with a T (timestamp) in the mapset name)
+    B_arr=($(g.list type=raster pattern=${B} mapset=* | grep "@.*T"))
+    r.mapcalc "${B} = null()" --o --q
+    for i in $SZA_LUT_idxs; do
+	echo "patching ${B} from ${B_arr[${i}]} [$i]"
+	r.mapcalc "${B} = if(SZA_LUT == ${i}, ${B_arr[${i}]}, ${B})" --o --q
+    done
+done
+
+# combine bands to make RGB
+r.composite -d -c blue=Oa04_reflectance green=Oa06_reflectance red=Oa08_reflectance output=RGB --o
+r.out.png input=RGB output=${OUTFOLDER}/${DATE}/RGB.png --o
+
+# save everything to disk
 TIFOPTS='type=Float32 createopt=COMPRESS=DEFLATE,PREDICTOR=2,TILED=YES --q --o'
-for B in $(echo ${BANDS}); do
-    r.patch input=$(g.list type=raster pattern="${B}_tmp_*" separator=,) output=${B} --o
+for B in $(echo ${BANDS} RGB); do
+    # r.patch input=$(g.list type=raster pattern="${B}_tmp_*" separator=,) output=${B} --o
     r.colors map=${B} color=grey
     r.out.gdal -c input=${B} output=${OUTFOLDER}/${DATE}/${B}.tif ${TIFOPTS}
 done
 
-r.composite -d -c blue=Oa04_reflectance green=Oa06_reflectance red=Oa08_reflectance output=RGB --o
-r.out.gdal -c input=RGB output=${OUTFOLDER}/${DATE}/RGB.tif ${TIFOPTS}
-r.out.png input=RGB output=${OUTFOLDER}/${DATE}/RGB.png --o
-
-g.remove -f type=raster pattern="*_tmp_*"
+# rm -fR /tmp/tmpG
