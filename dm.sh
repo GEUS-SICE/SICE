48dfa3a4312f7c25f2237d63e4263901d56892ce
Kenneth D. Mankoff <mankoff@gmail.com>
2019-08-26 16:21:14 +0200

    clean up debug statements

--- dm.sh
+++ dm.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-# ./dm.sh 20170801 ./tmp ./mosaic
-
 if [ "$#" -ne 3 ]; then
   echo "Usage: $0 <yyyymmdd> <infolder> <outfolder>" >&2
   exit 1
 fi
 
-DATE=$1
-INFOLDER=$2
-OUTFOLDER=$3
-
-# TESTING
-# DATE=20170827
-# INFOLDER=./tmp
-# OUTFOLDER=./mosaic
+DATE=$1      # YYYYMMDD
+INFOLDER=$2  # ./tmp ?
+OUTFOLDER=$3 # ./mosaic ?
 
 ## Work in /tmp/tmpG
 BASE=/tmp
@@ -25,8 +18,3 @@ grass -e -c mask.tif ${BASE}/${LOC}
 
 grass ${BASE}/${LOC}/PERMANENT --exec ./dm.grass.sh $DATE $INFOLDER $OUTFOLDER
 
-# debug:
-# grass72 /tmp/tmpG/PERMANENT
-# DATE=20170827
-# INFOLDER=./tmp
-# OUTFOLDER=./MOSAIC
