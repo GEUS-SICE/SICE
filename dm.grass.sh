a09648805c49441ac225e98e004414fc98275c82
Adrien Wehrlé <44843368+AdrienWehrle@users.noreply.github.com>
2020-04-23 16:38:16 +0200

    r_TOA_S5 r_TOA_S5_rc r_TOA_S1 don't exist without SCDA (#35)

--- dm.grass.sh
+++ dm.grass.sh
@@ -112,7 +112,7 @@ mapset_list=$(g.mapsets --q -l separator=newline | grep T | tr '\n' ','| sed 's/
 raster_list=$(g.list type=raster pattern=r_TOA_01 mapset=${mapset_list} separator=comma)
 r.series input=${raster_list} method=count output=num_scenes --q
 
-bandsFloat32="$(g.list type=raster pattern="r_TOA_*") SZA SAA OZA OAA WV O3 r_TOA_S5 r_TOA_S5_rc r_TOA_S1 height"
+bandsFloat32="$(g.list type=raster pattern="r_TOA_*") SZA SAA OZA OAA WV O3 height"
 bandsInt16="sza_lut num_scenes num_scenes_cloudfree"
 log_info "Writing mosaics to disk..."
 
