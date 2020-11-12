40d8a42b821fc1cd030deb3cd3adcb282042af4e
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2020-11-12 11:25:45 +0100

    Write SCDA_grow and SCDA_clump as outputs
    Until now the two variables were not correctly written out because removed before export.

--- dm.grass.sh
+++ dm.grass.sh
@@ -58,7 +58,7 @@ for scene in ${scenes}; do
   # SZA_CM is SZA but Cloud Masked: Invalid where buffered clouds over ice w/ valid SZA
   r.mapcalc "SZA_CM0 = if((isnull(SCDA_area) && (MASK@PERMANENT == 220)) || (isnull(SCDA_v20) && (MASK@PERMANENT != 220)), null(), 1)" --q
   r.mapcalc "SZA_CM = if(not(isnull(SZA)) & SZA_CM0, 1, null())" --q
-  g.remove -f type=raster name=SCDA_grow,SCDA_clump,SCDA_area,SZA_CM0 --q
+  g.remove -f type=raster name=SCDA_area,SZA_CM0 --q
 done
 
 # The target bands to mask for clouds (CM; all except r_TOA and BT)
@@ -125,7 +125,7 @@ mapset_list=$(g.mapsets --q -l separator=newline | grep T | tr '\n' ','| sed 's/
 raster_list=$(g.list type=raster pattern=r_TOA_01 mapset=${mapset_list} separator=comma)
 r.series input=${raster_list} method=count output=num_scenes --q
 
-bandsFloat32="$(g.list type=raster pattern="r_TOA_*") SZA SAA OZA OAA WV O3 NDSI BT_S7 BT_S8 BT_S9 r_TOA_S5 r_TOA_S5_rc r_TOA_S1 height SCDA_v20 SCDA_grow SCDA_clump SCDA_area"
+bandsFloat32="$(g.list type=raster pattern="r_TOA_*") SZA SAA OZA OAA WV O3 NDSI BT_S7 BT_S8 BT_S9 r_TOA_S5 r_TOA_S5_rc r_TOA_S1 height SCDA_v20 SCDA_grow SCDA_clump"
 bandsInt16="sza_lut_CM num_scenes num_scenes_cloudfree"
 log_info "Writing mosaics to disk..."
 
