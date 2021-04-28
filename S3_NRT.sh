7c09f72ab70a3d22cb7c89fb1bc0c4969ecf15a0
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2021-04-28 20:50:39 +0000

    NRT on PolarTEP

--- S3_NRT.sh
+++ S3_NRT.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 
-# Wrapper for running SICE pipeline Near Real-Time (NRT) 
+# Wrapper for running SICE pipeline Near Real-Time (NRT)
 
 # example of cron job:
 # m h  dom mon dow   command
 # 00 12 * * * /bin/bash -c "/path/to/wrapper/S3_NRT.sh" > /path/to/log/log_NRT.txt
 
 # use SNAP gpt
-export PATH=/path/to/snap/bin:${PATH}
+export PATH=/srv/home/8675309/local/snap/bin:${PATH}
 
 # activate SICE anaconda virtual environment
-. /path/to/anaconda/envs/SICE/bin/activate
+# . /srv/home/8675309/local/anaconda/bin/activate SICE
+
+# . /srv/home/8675309/local/anaconda/envs/SICE/bin/activate
+
+# source /path/to/anaconda/etc/profile.d/conda.sh
+# conda activate SICE
 
 set -o errexit
 set -o nounset
@@ -32,36 +37,38 @@ cd "${0%/*}"
 
 LD_LIBRARY_PATH=. # SNAP requirement
 
-date=$(date -d '-2days' "+%Y-%m-%d")
+date=$(date -d '-1days' "+%Y-%m-%d")
 year=$(date "+%Y")
 
-declare -a regions=("Greenland" "Iceland" "Svalbard" "NovayaZemlya" "SevernayaZemlya" "FransJosefLand" "NorthernArcticCanada" "SouthernArcticCanada" "JanMayen" "Norway" "Beaufort")
+declare -a regions=("Greenland")
 
 for region in "${regions[@]}"; do
 
-  # CREODIAS
-  SEN3_local=/eodata/Sentinel-3
-  SEN3_source=/sice-data/SICE_NRT/${region}/S3
-  proc_root=/sice-data/SICE_NRT/${region}/proc
-  mosaic_root=/sice-data/SICE_NRT/${region}/mosaic
+    # CREODIAS
+    SEN3_local=/eodata/Sentinel-3
+    SEN3_source=/sice-data/SICE_NRT21/${region}/S3
+    proc_root=/sice-data/SICE_NRT21/${region}/proc
+    mosaic_root=/sice-data/SICE_NRT21/${region}/mosaic
 
-  mkdir -p /sice-data/SICE_NRT/${region}
+    mkdir -p /sice-data/SICE_NRT21/${region}
 
-  ### Fetch one day of OLCI & SLSTR scenes over Greenland
-  ## Use local files (PTEP, DIAS, etc.)
-  ./dhusget_wrapper.sh -d ${date} -l ${SEN3_local} -o ${SEN3_source}/${year}/${date} \
-                       -f ${region} -u <user> -p <password>
+    ### Fetch one day of OLCI & SLSTR scenes over Greenland
+    ## Use local files (PTEP, DIAS, etc.)
+    ./dhusget_wrapper.sh -d ${date} -l ${SEN3_local} -o ${SEN3_source}/${year}/${date} \
+			 -f ${region} -u baptistevdx -p geus1234
 
-  # SNAP: Reproject, calculate reflectance, extract bands, etc.
-  ./S3_proc.sh -i ${SEN3_source}/${year}/${date} -o ${proc_root}/${date} -X S3_fast.xml -t
+    # SNAP: Reproject, calculate reflectance, extract bands, etc.
+    ./S3_proc.sh -i ${SEN3_source}/${year}/${date} -o ${proc_root}/${date} -X S3_fast.xml -t
 
-  # Run the Simple Cloud Detection Algorithm (SCDA)
-  python ./SCDA.py ${proc_root}/${date}
+    # Run the Simple Cloud Detection Algorithm (SCDA)
+    python ./SCDA.py ${proc_root}/${date}
 
-  # Mosaic
-  ./dm.sh ${date} ${proc_root}/${date} ${mosaic_root}
+    # Mosaic
+    ./dm.sh ${date} ${proc_root}/${date} ${mosaic_root}
 
-  # SICE
-  python ./sice.py ${mosaic_root}/${date}
+    # SICE
+    python ./sice.py ${mosaic_root}/${date}
 
+    # upload to SICE dataverse
+    python ./SICE_dataverse_upload.py ${mosaic_root}/${date}
 done
