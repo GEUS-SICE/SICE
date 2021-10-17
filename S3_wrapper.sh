bf621dab4d0ad6889073b7eff609043dc0e05bdd
Baptiste Vandecrux <b.vandecrux@gmail.com>
2021-10-18 01:36:21 +0200

    Cleaned S3_wrapper.sh Login info now in scihub_login_information.txt Updated the I/O for slopey

--- S3_wrapper.sh
+++ S3_wrapper.sh
@@ -12,20 +12,18 @@ log_info() { echo -e "${green}[$(date --iso-8601=seconds)] [INFO] ${@}${nc}"; }
 log_warn() { echo -e "${orange}[$(date --iso-8601=seconds)] [WARN] ${@}${nc}"; }
 log_err() { echo -e "${red}[$(date --iso-8601=seconds)] [ERR] ${@}${nc}" 1>&2; }
 
-# CREODIAS
-# SEN3_local=/eodata/Sentinel-3
-# SEN3_source=~/sice-data/SICE/S3
-# proc_root=~/sice-data/SICE/proc
-# mosaic_root=~/sice-data/SICE/mosaic
-### dev
-SEN3_source=./SEN3
-SEN3_local=./SEN3
-proc_root=./out
-mosaic_root=./mosaic
+# scihub login information
+IFS=' ' read -r username password < scihub_login_info.txt
+
+# source and destination folder
+SEN3_local=/eodata/Sentinel-3 # where the '.SEN3' files are
+SEN3_source=/eodata/Sentinel-3 
+proc_root=~/sice-data/proc # where the scenes' geotiff will be saved
+mosaic_root=~/sice-data/mosaic # where the output tiff will be saved
 
 # Geographic area
 area=Svalbard
-# Svalbard...
+# list: Svalbard, Greenland, NovayaZemlya, SevernayaZemlya, Iceland, FransJosefLand, NorthernArcticCanada, SouthernArcticCanada, JanMayen, Norway, Beaufort, AlaskaYukon
 
 # Slope correction 
 slopey=false
@@ -34,19 +32,19 @@ slopey=false
 error=false
 
 # Fast processing:
-fast=false
+fast=true
 
 if [ "$fast" = true ] ; then
   # so far the only speed up done is to not extract all bands
   xml_file=S3_fast.xml
 else
-  xml_file=S3_fast.xml
+  xml_file=S3.xml
 fi
 
 LD_LIBRARY_PATH=. # SNAP requirement
 
 # for year in 2017 ; do
-#   for doy in $(seq -w 91 276); do
+#     for doy in $(seq -w 91 276); do
 
 ### DEBUG
 for year in 2021; do
@@ -61,9 +59,9 @@ for year in 2021; do
     
     ### Fetch one day of OLCI & SLSTR scenes over Greenland
     ## Use local files (PTEP, DIAS, etc.)
-    # ./dhusget_wrapper.sh -d ${date} -l ${SEN3_local} -o ${SEN3_source}/${year}/${date} -f ${area} -u baptistevdx -p geus1234 || error=true
+     ./dhusget_wrapper.sh -d ${date} -l ${SEN3_local} -o ${SEN3_source}/${year}/${date} -f ${area} -u username -p password || error=true
     ## Download files
-     ./dhusget_wrapper.sh -d ${date} -o ${SEN3_source}/${year}/${date}  -f ${area} -u baptistevdx -p geus1234 || error=true
+    # ./dhusget_wrapper.sh -d ${date} -o ${SEN3_source}/${year}/${date}  -f ${area} -u username -p password || error=true
     
     # SNAP: Reproject, calculate reflectance, extract bands, etc.
     ./S3_proc.sh -i ${SEN3_source}/${year}/${date} -o ${proc_root}/${date} -X ${xml_file} || error=true -t
@@ -76,11 +74,30 @@ for year in 2021; do
     
     if [ "$slopey" = true ] ; then
       # Run the slopey correction
-      python ./get_ITOAR.py ${mosaic_root}/${date}/ $(pwd)/ArcticDEM/  || error=true
+      python ./get_ITOAR.py ${mosaic_root}/${date}/ ./ArcticDEM/  || error=true
+      # saving uncorrected files
+      cp -f ${mosaic_root}/${date}/SZA.tif ${mosaic_root}/${date}/SZA_org.tif      
+      cp -f ${mosaic_root}/${date}/OZA.tif ${mosaic_root}/${date}/OZA_org.tif
+      cp -f ${mosaic_root}/${date}/r_TOA_17.tif ${mosaic_root}/${date}/r_TOA_17_org.tif
+      cp -f ${mosaic_root}/${date}/r_TOA_21.tif ${mosaic_root}/${date}/r_TOA_21_org.tif
+
+      # overwriting with corrected files
+      cp -f ${mosaic_root}/${date}/SZA_eff.tif ${mosaic_root}/${date}/SZA.tif      
+      cp -f ${mosaic_root}/${date}/OZA_eff.tif ${mosaic_root}/${date}/OZA.tif
+      cp -f ${mosaic_root}/${date}/ir_TOA_17.tif ${mosaic_root}/${date}/r_TOA_17.tif
+      cp -f ${mosaic_root}/${date}/ir_TOA_21.tif ${mosaic_root}/${date}/r_TOA_21.tif
     fi
 
     # SICE
     python ./sice.py ${mosaic_root}/${date} || error=true
     
+    if [ "$slopey" = true ] ; then
+      # restoring uncorrected files
+      cp -f ${mosaic_root}/${date}/SZA_org.tif ${mosaic_root}/${date}/SZA.tif      
+      cp -f ${mosaic_root}/${date}/OZA_org.tif ${mosaic_root}/${date}/OZA.tif
+      cp -f ${mosaic_root}/${date}/r_TOA_17_org.tif ${mosaic_root}/${date}/r_TOA_17.tif
+      cp -f ${mosaic_root}/${date}/r_TOA_21_org.tif ${mosaic_root}/${date}/r_TOA_21.tif
+      rm ${mosaic_root}/${date}/SZA.tif ${mosaic_root}/${date}/OZA.tif ${mosaic_root}/${date}/r_TOA_17.tif ${mosaic_root}/${date}/r_TOA_21.tif
+    fi
   done
 done
