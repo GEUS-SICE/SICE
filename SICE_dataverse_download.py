7f2d4b55d270a8a4784fad5b67f459062df08578
AdrienWehrle <adrien.wehrle@hotmail.fr>
2021-12-19 19:16:42 +0100

    fixing data set choice

--- SICE_dataverse_download.py
+++ SICE_dataverse_download.py
@@ -22,13 +22,14 @@ import os
 import pandas as pd
 from pyDataverse.api import NativeApi, DataAccessApi
 
-# choose SICE dataset
-dataset_name = 'SICE_NRT'
+# %% choose SICE dataset
 
-if dataset_name == 'SICE_NRT':
+dataset_name = "SICE_NRT"
+
+if dataset_name == "SICE_NRT":
     # dataset DOI
     persistentId = "doi:10.22008/FK2/SD56CB"
-    
+
     # list of output files that needs to be downloaded from each dataverse subfolder
     files_to_download = [
         "r_TOA_01.tif",
@@ -40,7 +41,7 @@ if dataset_name == 'SICE_NRT':
         "SCDA_final.tif",
     ]
 
-if dataset_name = 'SICE_BBA':
+if dataset_name == "SICE_BBA":
     # dataset DOI
     persistentId = "doi:10.22008/FK2/A5RXJ5"
 
@@ -74,7 +75,7 @@ date_list = list(pd.date_range(date_range[0], date_range[1]).strftime("%Y-%m-%d"
 # %% the actual download
 
 #  local folder to store files
-destination_folder = f'{folder_store}/{dataset_name}'
+destination_folder = f"{folder_store}/{dataset_name}"
 os.makedirs(destination_folder)
 
 # access list of files to download from the dataverse
@@ -85,24 +86,29 @@ for file in dataverse_files:
     # store metadata
     dataverse_filename = file["dataFile"]["filename"]
     file_id = file["dataFile"]["id"]
-    if dataset_choice==0:
-        file_directory = file["directoryLabel"]
 
-    # if dataverse_filename in files_to_download and file_directory in date_list:
-    if dataverse_filename in files_to_download and file_directory in date_list:
+    # extract SICE_NRT file date from subfolder name
+    if dataset_name == "SICE_NRT":
+        file_date = file["directoryLabel"]
+
+    # etract SICE_BBA file date from file name
+    if dataset_name == "SICE_BBA":
+        file_date = file["directoryLabel"]
+
+    if dataverse_filename in files_to_download and file_date in date_list:
 
         # create the dataverse directory tree
         try:
-            os.makedirs(f"{destination_folder}/{file_directory}")
+            os.makedirs(f"{destination_folder}/{file_date}")
         except FileExistsError:
             pass
 
-        local_filename = f"{destination_folder}{file_directory}/{dataverse_filename}"
+        local_filename = f"{destination_folder}{file_date}/{dataverse_filename}"
 
         # download if file does not exist yet
         if bool(os.path.isfile(local_filename)) == False:
 
-            print(f"downloading {file_directory}/{dataverse_filename}...")
+            print(f"downloading {file_date}/{dataverse_filename}...")
 
             # download datafile
             response = data_api.get_datafile(file_id)
