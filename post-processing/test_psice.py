bcba92b97f1a8bc92308ef293c5800a2ea31c74b
Adrien Wehrlé <adrien.wehrle@hotmail.fr>
2022-02-12 08:53:57 +0100

    Update and rename test_sice-popaviz.py to test_psice.py

--- post-processing/test_psice.py
+++ post-processing/test_psice.py
@@ -6,14 +6,14 @@
 
 """
 
-from sice_pp import SICEPostProcessing
+from psice import PostSICE
 
 dataset_path = '/media/adrien/Elements/show_case_v14'
 
 # %% check initialization
 
 # one region, default variables
-d1 = SICEPostProcessing(dataset_path=dataset_path, regions=['Greenland'])
+d1 = PostSICE(dataset_path=dataset_path, regions=['Greenland'])
 assert isinstance(d1.files, dict)
 assert len(d1.files) == 1
 assert list(d1.files['Greenland'].keys()) == ['albedo_bb_planar_sw', 'snow_specific_surface_area']
@@ -21,7 +21,7 @@ assert len(d1.files['Greenland']['albedo_bb_planar_sw']) > 2
 assert len(d1.files['Greenland']['snow_specific_surface_area']) > 2
 
 # two regions, default variables
-d2 = SICEPostProcessing(dataset_path=dataset_path, regions=['Greenland', 'Iceland'])
+d2 = PostSICE(dataset_path=dataset_path, regions=['Greenland', 'Iceland'])
 assert isinstance(d2.files, dict)
 assert len(d2.files) == 2
 assert list(d2.files['Iceland'].keys()) == ['albedo_bb_planar_sw', 'snow_specific_surface_area']
@@ -29,7 +29,7 @@ assert len(d2.files['Iceland']['albedo_bb_planar_sw']) > 2
 assert len(d2.files['Iceland']['snow_specific_surface_area']) > 2
 
 # all available regions, default variables
-d3 = SICEPostProcessing(dataset_path=dataset_path)
+d3 = PostSICE(dataset_path=dataset_path)
 assert isinstance(d3.files, dict)
 assert len(d3.files) == 10
 assert list(d3.files['NovayaZemlya'].keys()) == ['albedo_bb_planar_sw', 'snow_specific_surface_area']
