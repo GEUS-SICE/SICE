7e3b6af50adc3d7aead2b71ed4995ac083f8ec92
AdrienWehrle <adrien.wehrle@hotmail.fr>
2022-02-12 15:58:25 +0000

    credentials from local authfile

--- S3_NRT.sh
+++ S3_NRT.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Wrapper for running SICE pipeline Near Real-Time (NRT) 
+# Wrapper for running SICE pipeline Near Real-Time (NRT)
 
 # example of cron job:
 # m h  dom mon dow   command
@@ -10,9 +10,10 @@
 export PATH=/path/to/snap/bin:${PATH}
 
 # activate SICE anaconda virtual environment
-. /path/to/anaconda/envs/SICE/bin/activate
+anaconda_path=""
+. "${anaconda_path}"/envs/SICE/bin/activate
 
-source /path/to/anaconda/etc/profile.d/conda.sh
+source "${anaconda_path}"/etc/profile.d/conda.sh
 conda activate SICE
 
 set -o errexit
@@ -20,10 +21,17 @@ set -o nounset
 set -o pipefail
 set -x
 
-red='\033[0;31m'; orange='\033[0;33m'; green='\033[0;32m'; nc='\033[0m' # No Color
-log_info() { echo -e "${green}[$(date --iso-8601=seconds)] [INFO] ${@}${nc}"; }
-log_warn() { echo -e "${orange}[$(date --iso-8601=seconds)] [WARN] ${@}${nc}"; }
-log_err() { echo -e "${red}[$(date --iso-8601=seconds)] [ERR] ${@}${nc}" 1>&2; }
+red='\033[0;31m'
+orange='\033[0;33m'
+green='\033[0;32m'
+nc='\033[0m' # No Color
+log_info() { echo -e "${green}[$(date --iso-8601=seconds)] [INFO] ${*}${nc}"; }
+log_warn() { echo -e "${orange}[$(date --iso-8601=seconds)] [WARN] ${*}${nc}"; }
+log_err() { echo -e "${red}[$(date --iso-8601=seconds)] [ERR] ${*}${nc}" 1>&2; }
+
+# Scihub credentials (from local auth.txt in SICE folder)
+username=$(sed -n '1p' auth.txt)
+password=$(sed -n '2p' auth.txt)
 
 # change directory to the current folder
 cd "${0%/*}"
@@ -42,29 +50,29 @@ declare -a regions=("Greenland" "Iceland" "Svalbard" "NovayaZemlya" "SevernayaZe
 
 for region in "${regions[@]}"; do
 
-  # CREODIAS
-  SEN3_local=/eodata/Sentinel-3
-  SEN3_source=/sice-data/SICE/${region}/S3
-  proc_root=/sice-data/SICE/${region}/proc
-  mosaic_root=/sice-data/SICE/${region}/mosaic
+	# CREODIAS
+	SEN3_local=/eodata/Sentinel-3
+	SEN3_source=/sice-data/SICE/"${region}"/S3
+	proc_root=/sice-data/SICE/"${region}"/proc
+	mosaic_root=/sice-data/SICE/"${region}"/mosaic
 
-  mkdir -p /sice-data/SICE/${region}
+	mkdir -p /sice-data/SICE/"${region}"
 
-  ### Fetch one day of OLCI & SLSTR scenes over Greenland
-  ## Use local files (PTEP, DIAS, etc.)
-  ./dhusget_wrapper.sh -d ${date} -l ${SEN3_local} -o ${SEN3_source}/${year}/${date} \
-                       -f ${region} -u <user> -p <password>
+	### Fetch one day of OLCI & SLSTR scenes over Greenland
+	## Use local files (PTEP, DIAS, etc.)
+	./dhusget_wrapper.sh -d "${date}" -l "${SEN3_local}" -o "${SEN3_source}"/"${year}"/"${date}" \
+		-f "${region}" -u "${username}" -p "${password}"
 
-  # SNAP: Reproject, calculate reflectance, extract bands, etc.
-  ./S3_proc.sh -i ${SEN3_source}/${year}/${date} -o ${proc_root}/${date} -X S3_fast.xml -t
+	# SNAP: Reproject, calculate reflectance, extract bands, etc.
+	./S3_proc.sh -i "${SEN3_source}"/"${year}"/"${date}" -o "${proc_root}"/"${date}" -X S3_fast.xml -t
 
-  # Run the Simple Cloud Detection Algorithm (SCDA)
-  python ./SCDA.py ${proc_root}/${date}
+	# Run the Simple Cloud Detection Algorithm (SCDA)
+	python ./SCDA.py "${proc_root}"/"${date}"
 
-  # Mosaic
-  ./dm.sh ${date} ${proc_root}/${date} ${mosaic_root}
+	# Mosaic
+	./dm.sh "${date}" "${proc_root}"/"${date}" "${mosaic_root}"
 
-  # SICE
-  python ./sice.py ${mosaic_root}/${date}
+	# SICE
+	python ./sice.py "${mosaic_root}"/"${date}"
 
 done
